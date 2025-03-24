497590beb274ef075862adcd5bc6f790da86b3b6
honeydewdrop <smadam26@g.holycross.edu>
2025-03-24 18:00:31 -0400

    fixed ingredient bug

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ce3b56fc441a17ad31fd14d4d98a580ca89f637")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab4fe45dac0d6fe0b469927c10a500c341517201")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
