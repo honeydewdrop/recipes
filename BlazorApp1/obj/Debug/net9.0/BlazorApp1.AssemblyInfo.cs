7c3b5e071209f02360a65cc7b083b0e8f7bc662c
honeydewdrop <smadam26@g.holycross.edu>
2025-04-07 21:06:12 -0400

    added sorted recipe view

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6775107d5892c686136273886f7f962a2c38cd2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+638fa68e03174d04ced21c3ace58a79910499afc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
