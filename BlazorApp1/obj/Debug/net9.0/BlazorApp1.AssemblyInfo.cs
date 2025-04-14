e63b376405ef39cf203f62fe24c7faaa51473506
Lenny-Tran <150497986+Lenny-Tran@users.noreply.github.com>
2025-04-14 00:57:36 -0400

    Fattiness taste
    Added fattiness taste and comments

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b51856e968828499113c4bc8b2730d9b7c09d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa18c8a39bbc1f36b4745cf20244d4d1a49cb19")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
