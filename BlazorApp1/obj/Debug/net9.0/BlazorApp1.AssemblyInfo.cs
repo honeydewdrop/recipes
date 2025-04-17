f3b9a2414445e96f51bc499f3f7156b1f0660c5c
honeydewdrop <smadam26@g.holycross.edu>
2025-04-17 18:55:48 -0400

    comments

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f808c329ec1517e274e7ead9dfa573abaf2882")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a7693ae3c43a4919e879eaee4c04d0db200f4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
