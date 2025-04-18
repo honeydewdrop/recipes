c6e154817dec89a124141de78716860b2ea92d89
honeydewdrop <smadam26@g.holycross.edu>
2025-04-18 01:17:05 -0400

    fixed dropdown bug

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a7693ae3c43a4919e879eaee4c04d0db200f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd65f66a1fac61fe7e524dc1df9a8ed79f80c6e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
