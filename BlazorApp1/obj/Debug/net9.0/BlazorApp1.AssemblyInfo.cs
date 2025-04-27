4230c754cc1680d9a983b770e18dccdec87c53ff
honeydewdrop <smadam26@g.holycross.edu>
2025-04-27 13:57:37 -0400

    marked changes

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd65f66a1fac61fe7e524dc1df9a8ed79f80c6e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e154817dec89a124141de78716860b2ea92d89")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
